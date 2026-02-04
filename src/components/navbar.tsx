161e425430897b65928da6ddfc51bfb76da4ffa2
YugandharrPatil <yugandhar.patil@learner.manipal.edu>
2026-02-04 23:16:59 +0530

    blog page removed all blogs on home page. dynamic route fixed to match that. blogs style changed. navbar removed.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,9 +2,12 @@ import { Button } from "./ui/button";
 
 export default function Navbar() {
 	return (
-		<nav className="flex h-20 items-center justify-between">
-			<div className="bg-red-500 w-12 h-12">Logo</div>
-			<div className="flex gap-2">
+		<nav className="h-20 container mx-auto flex items-center">
+			<h1 className="font-bold text-3xl">
+				Yugz
+				<span className="text-orange-400">.</span>
+			</h1>
+			{/* <div className="flex gap-2">
 				<Button asChild>
 					<a href="/">Home</a>
 				</Button>
@@ -13,7 +16,7 @@ export default function Navbar() {
 					<a href="/blog">Blog</a>
 				</Button>
 			</div>
-			<div className="bg-red-500 w-12 h-12">Logo</div>
+			<div className="bg-red-500 w-12 h-12">Logo</div> */}
 		</nav>
 	);
 }
