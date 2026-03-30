5182fa903f7238cdef7f00f52faac3c359beccf8
YugandharrPatil <yugandhar.patil@learner.manipal.edu>
2026-03-30 23:41:02 +0530

    a tag replaced with custom link component to open in a new tab

--- src/components/mdx/components.ts
+++ src/components/mdx/components.ts
@@ -1,5 +1,7 @@
 import BlockQuote from "@/components/mdx/blockquote.astro";
+import Link from "@/components/mdx/link.astro";
 
 export const components = {
-	blockquote: BlockQuote,
+	// blockquote: BlockQuote,
+	a: Link,
 };
